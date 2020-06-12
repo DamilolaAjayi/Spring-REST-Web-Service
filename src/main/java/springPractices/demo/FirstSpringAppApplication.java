c1c093a0a6b693816d62bad5e0db0de95fbc1760
Damilola Ajayi <ajayidamey@gmail.com>
2020-06-13 00:02:51 +0100

    Updating Files

---
+++ src/main/java/springPractices/demo/FirstSpringAppApplication.java
@@ -0,0 +1,13 @@
+package springPractices.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringAppApplication.class, args);
+	}
+
+}
