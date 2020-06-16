0a53674eadc8c06260cfd283ac3c5440b9784e38
Damilola Ajayi <ajayidamey@gmail.com>
2020-06-16 14:51:32 +0100

    Update FakePersonDataAccessService.java

--- src/main/java/springPractices/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/springPractices/demo/dao/FakePersonDataAccessService.java
@@ -1,20 +1,21 @@
-package springPractices.demo.dao;
+package com.rensource.demo.DAO;
 
+import com.rensource.demo.Model.Person;
 import org.springframework.stereotype.Repository;
-import springPractices.demo.model.Person;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@Repository("fakeDao")
-public class FakePersonDataAccessService implements PersonDao {
+@Repository("fakeDAO")
+public class FakePersonDataAccessService implements PersonDao{
+
     private static List<Person> DB = new ArrayList<>();
 
     @Override
     public int insertPerson(UUID id, Person person) {
-        DB.add(new Person(id, person.getName()));
+        DB.add(new Person(id,person.getName()));
         return 1;
     }
 
@@ -25,13 +26,16 @@ public class FakePersonDataAccessService implements PersonDao {
 
     @Override
     public Optional<Person> selectPersonById(UUID id) {
-        return DB.stream().filter(person -> person.getId().equals(id)).findFirst();
+        return DB.stream()
+                .filter(person -> person.getId().equals(id))
+                .findFirst();
+
     }
 
     @Override
     public int deletePersonById(UUID id) {
         Optional<Person> personMaybe = selectPersonById(id);
-        if(!personMaybe.isPresent()){
+        if(personMaybe.isEmpty()){
             return 0;
         }
         DB.remove(personMaybe.get());
@@ -39,15 +43,19 @@ public class FakePersonDataAccessService implements PersonDao {
     }
 
     @Override
-    public int updatePersonById(UUID id, Person update) {
+    public int updatePersonById(UUID id, Person newPerson) {
         return selectPersonById(id)
                 .map(person -> {
                     int indexOfPersonToUpdate = DB.indexOf(person);
-                    if(indexOfPersonToUpdate == 0){
-                        DB.set(indexOfPersonToUpdate, new Person(id, update.getName()));
+                    if(indexOfPersonToUpdate >= 0){
+                        DB.set(indexOfPersonToUpdate, new Person(id, newPerson.getName()));
+                        return 1;
+                    }else {
+                        return 0;
                     }
-                    return 0;
                 })
                 .orElse(0);
     }
+
+
 }
